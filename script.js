e0fb9562128e58ab55d3340b1ec6556eca88cc53
Carlos Llorente <llorenteyuste@hotmail.com>
2018-10-23 23:28:25 +0200

    grafico

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
 
-// var margin = {top: 20, right: 20, botton:20, left:20},
-    width = 500 
-    height = 500
-    radius = width/2;
+  var data = {
+    labels: ["Smartphone", "Tablet"],
+    datasets: [
+          {
+            label: "REVENUE",
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+            data: [80000,120000]
+          },
+          {
+            label: "IMPRESIONS",
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+            data: [30000000,20000000]
+          },
+          {
+            label: "VISITS",
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+            data: [120000000,480000000]
+          },
+
+        ]
+  };
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: data
+});
+
+// https://developer.mozilla.org/es/docs/Learn/JavaScript/Objects/JSON
+
+// // var margin = {top: 20, right: 20, botton:20, left:20},
+//     width = 500 
+//     height = 500
+//     radius = width/2;
  
-    var arc = d3.arc()
-    .outerRadius(radius-10)
-    .innerRadius(0); 
+//     var arc = d3.arc()
+//     .outerRadius(radius-10)
+//     .innerRadius(0); 
  
-    var labelArc = d3.arc()
-        .outerRadius(radius -50)
-        .innerRadius(radius -50); 
+//     var labelArc = d3.arc()
+//         .outerRadius(radius -50)
+//         .innerRadius(radius -50); 
 
-    var pie = d3.pie()
-    .sort (null)
-    .value(function (d){return d.count; });
+//     var pie = d3.pie()
+//     .sort (null)
+//     .value(function (d){return d.count; });
 
-    var svg = d3.select ("grafico").append ("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width/2 +","+ height/2 + ")"); 
+//     var svg = d3.select ("grafico").append ("svg")
+//         .attr("width", width)
+//         .attr("height", height)
+//         .append("g")
+//         .attr("transform", "translate(" + width/2 +","+ height/2 + ")"); 
 
-    // import data  
-    d3.json("data.json", function(error, json){ // <-F
-        data = data.concat(json);
-        render(data);
+//     // import data  
+//     d3.json("data.json", function(error, json){ // <-F
+//         data = data.concat(json);
+//         render(data);
     //     if (error) throw error;
     //     //parse the data 
     //     data.forEach(function(d) {
@@ -48,8 +77,4 @@
     //     .attr("dy", ".35em")
     //     .text(function(d) {return d.data.smartphone;} )
     //     .text(function(d) {return d.data.tablet;} );
-    
-
-    })
-
-    
+    // })
